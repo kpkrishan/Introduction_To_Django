51a19144dfc8ca32faa212fab95c8664e1c6a02a
kpkrishan <krishan.007305@gmail.com>
2020-07-07 19:45:50 +0530

    Add more html tags

--- Textutils/Textutils/views.py
+++ Textutils/Textutils/views.py
@@ -12,13 +12,13 @@ def removepunc(request):
     return HttpResponse("<h1>Remove punctuation!</h1>")
 
 def capfirst(request):
-    return HttpResponse("<h2>Capitalize</h2>")
+    return HttpResponse(''' <h2>Capitalize</h2> <a href='https://github.com/kpkrishan?tab=repositories'>My Github</a> ''')
 
 def newline(request):
-    return HttpResponse("add newline")
+    return HttpResponse('<h3>add newline</h3>')
 
 def spaceremover(request):
-    return HttpResponse("It removes the space")
+    return HttpResponse(''' 'It removes the space' <a href='https://www.youtube.com/watch?v=0dBZZOYIDW0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=9'> Django tutorial</a>''')
 
 def charcount(request):
     return HttpResponse("counting the character!")
