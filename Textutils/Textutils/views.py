4443a28ddc8945aabb79973450807b3d1206a300
kpkrishan <krishan.007305@gmail.com>
2020-07-08 08:31:54 +0530

    Add back buttons

--- Textutils/Textutils/views.py
+++ Textutils/Textutils/views.py
@@ -9,16 +9,16 @@ def home(request):
 #textutil pipeline
 
 def removepunc(request):
-    return HttpResponse("<h1>Remove punctuation!</h1>")
+    return HttpResponse('''<h1>Remove punctuation!</h1>  <a href='/'> Back</a>''')
 
 def capfirst(request):
-    return HttpResponse(''' <h2>Capitalize</h2> <a href='https://github.com/kpkrishan?tab=repositories'>My Github</a> ''')
+    return HttpResponse(''' <h2>Capitalize</h2> <a href='https://github.com/kpkrishan?tab=repositories'>My Github</a>  <a href='/'> Back</a>''')
 
 def newline(request):
-    return HttpResponse('<h3>add newline</h3>')
+    return HttpResponse('''<h3>add newline</h3> <a href='/'> Back</a>''')
 
 def spaceremover(request):
-    return HttpResponse(''' 'It removes the space' <a href='https://www.youtube.com/watch?v=0dBZZOYIDW0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=9'> Django tutorial</a>''')
+    return HttpResponse(''' 'It removes the space' <a href='https://www.youtube.com/watch?v=0dBZZOYIDW0&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=9'> Django tutorial</a> <a href='/'> Back</a>''')
 
 def charcount(request):
-    return HttpResponse("counting the character!")
+    return HttpResponse('''<h1>counting the character!</h1> <a href='/'> Back</a>''')
